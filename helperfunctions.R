4d6deb6a3bcba61b4d6a6fbe6b64da9be4c3c037
Aadish Chopra <aadishchopra@rediffmail.com>
2019-06-30 18:15:26 -0500

    data cleansing

--- helperfunctions.R
+++ helperfunctions.R
@@ -4,16 +4,12 @@ removethesecolumns<-function(pattern_remove,dataset)
   message(" removing column ")
   message(grep(pattern = pattern_remove,x = names(dataset),value = T))
   dataset %>% select(-contains(pattern_remove,ignore.case = TRUE))
-  
 }
-
 
 analyze_distinct_values<-function(column)
 {
   length(unique(column))
 }
-
-
 
 strip_money<-function(dataset,pattern){
   colnames(dataset[grep(pattern,colnames(dataset))])
