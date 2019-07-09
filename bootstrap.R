9bc5c032555a4490c7dd8f4f27c9e4c431ecee42
Aadish Chopra <aadishchopra@rediffmail.com>
2019-07-09 18:24:26 -0500

    forecasting

--- bootstrap.R
+++ bootstrap.R
@@ -3,7 +3,7 @@
 # otherwise we can clone the repository and then do a pull 
 # Load all the libraries 
 
-required_packages<-c('knitr','dplyr','htmlTable','stringr','ggplot2','gganimate')
+required_packages<-c('knitr','dplyr','htmlTable','stringr','ggplot2','prophet')
 load_required_packages<-function(required_packages){
   if(!require(required_packages,character.only = TRUE))
   {
